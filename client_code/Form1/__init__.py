0cf4920de69dab89c1059e6d417d99a2d2b6d7e4
jamie.gostin@kds.vic.edu.au <jamie.gostin@kds.vic.edu.au>
2024-06-24 02:05:01 +0000

    Created Form1.ItemTemplate1; Edited settings, theme/parameters.yaml and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -8,3 +8,7 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.text_box_1.hide_text = not self.text_box_1.hide_text
