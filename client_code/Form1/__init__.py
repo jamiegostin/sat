32ac43cd5afb133bd55918120d5a54f37129b7f9
jamie.gostin@kds.vic.edu.au <jamie.gostin@kds.vic.edu.au>
2024-06-25 02:39:56 +0000

    Created Form2 and Form3; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,7 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
 
-
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
@@ -12,3 +11,18 @@ class Form1(Form1Template):
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
     self.text_box_1.hide_text = not self.text_box_1.hide_text
+    if self.text_box_1.hide_text:
+      self.button_1.icon = 'fa:eye-slash'
+    else:
+      self.button_1.icon = 'fa:eye'
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('Form2')
+
+  def btn_log_in_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    if self.text_box_username.text == '' or self.text_box_1.text == '':
+      alert('You must enter a username and password.')
+    else:
+      open_form('Form3')
